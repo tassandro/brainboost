9e718af0ca3b71fc765ff29065f325968b6403f9
Petralanda <vitorluispetralanda@gmail.com>
2025-03-25 09:03:01 -0300

    versão do html,css atualizados

---
+++ script.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal('.header', {
+    duration: 2000,
+
+});
+
+ScrollReveal().reveal('.hero-image', {
+    origin: 'left',
+    duration: 2000,
+    distance: '10%'
+});
+
+ScrollReveal().reveal('.hero-content', {
+    origin: 'right',
+    duration: 2000,
+    distance: '10%'
+});
